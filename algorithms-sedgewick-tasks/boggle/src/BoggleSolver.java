ea34af7795f95be1ffea27d89b9cc5eb32da3664
Petro Verheles <petro.verheles@gmail.com>
2019-09-09 17:14:46 +0300

    boggle task

--- algorithms-sedgewick-tasks/boggle/src/BoggleSolver.java
+++ algorithms-sedgewick-tasks/boggle/src/BoggleSolver.java
@@ -1,19 +1,42 @@
+import java.util.*;
+
 public class BoggleSolver
 {
+    private Set<String> words = new HashSet<>();
+
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
     public BoggleSolver(String[] dictionary) {
-
+        words.addAll(Arrays.asList(dictionary));
     }
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board) {
-        return null;
+        boolean[][] visited = new boolean[board.rows()][board.cols()];
+
+        List<String> result = new ArrayList<>();
+
+
+
+        return new ArrayList<>();
     }
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
     public int scoreOf(String word) {
-        return 0;
+        int length = word.length();
+
+        if (word.length() < 3) {
+            return 0;
+        }
+
+        switch (length) {
+            case 3:
+            case 4: return 1;
+            case 5: return 2;
+            case 6: return 3;
+            case 7: return 5;
+            default: return 11;
+        }
     }
 }
